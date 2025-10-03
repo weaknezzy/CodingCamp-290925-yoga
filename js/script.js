c49f5e33236bb52ddcc007e42c63422150058686
Yoga Pradana <132411206+weaknezzy@users.noreply.github.com>
2025-10-03 22:03:28 +0700

    menyelesaikan fitur tambah

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ function addTodo() {
     //mendapatkan value dari form
     const todoInput = document.getElementById("todo-input");
     const todoDate = document.getElementById("todo-date");
+    const todoListBody = document.getElementById('todo-list-body');
+
 
     //validasi input
     if (validateInput (todoInput.value, todoDate.value)) {
@@ -21,20 +23,51 @@ function addTodo() {
 function renderTodo() { 
 
     //mendapatkan id todo-list
-    const todoList = document.getElementById("todo-list");
+    const todoList = document.getElementById("todo-list-body");
 
     //mengosongkan todo-list
     todoList.innerHTML = "";
     
-    //menambahkan todo
+    //menambahkan todo ke table
     todos.forEach((todo,index) => {
-        todoList.innerHTML += `<li class="border p-2 mb-2 flex justify-between items-center">
-            <div>
-                <p class="font-bold">${todo.nama}</p>
-                <p class="text-sm text-gray-500">${todo.tanggal}</p>
-            </div>
-            <button type="button" onclick ="deleteTodo(${index})" class="bg-red-500 rounded text-white p-1">Hapus</button>
-        </li>`;
+        todoList.innerHTML += `<tr class="hover:bg-slate-600 transition duration-150 ">
+            
+            <td class="px-3 py-4 whitespace-nowrap font-medium text-slate-200">
+                ${todo.nama}
+            </td>
+            
+            <td class="px-3 py-4 whitespace-nowrap text-sm text-slate-400">
+                ${todo.tanggal}
+            </td>
+            
+            <td class="px-3 py-4 whitespace-nowrap">
+                <span class="bg-yellow-600 text-white text-xs font-bold px-2 py-1 rounded-full">
+                    Pending 
+                </span>
+            </td>
+            
+            <td class="px-3 py-4 whitespace-nowrap">
+                <button 
+                    type="button" 
+                    onclick="deleteTodo(${index})" 
+                    class="bg-red-500 hover:bg-red-600 transition duration-150 rounded text-white text-xs px-2 py-1">
+                    Hapus
+                </button>
+                <button 
+                    type="button" 
+                    onclick="deleteTodo(${index})" 
+                    class="bg-yellow-500 hover:bg-yellow-600 transition duration-150 rounded text-white text-xs px-2 py-1">
+                    Edit
+                </button>
+                 <button 
+                    type="button" 
+                    onclick="deleteTodo(${index})" 
+                    class="bg-green-500 hover:bg-green-600 transition duration-150 rounded text-white text-xs px-2 py-1">
+                    Selesaikan
+                </button>
+            </td>
+        </tr>
+        `;
     });
 }
 
